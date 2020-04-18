c05cf7cdf65a65f2d561f00a2201dce273ba7db8
Olfran Jiménez <olfran@gmail.com>
2020-04-18 17:55:38 +0200

    Replace canvas with SVG

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.ts',
@@ -9,7 +10,11 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" }
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: require.resolve('snapsvg/dist/snap.svg.js'),
+        use: 'imports-loader?this=>window,fix=>module.exports=0',
+      }
     ]
   },
   output: {
