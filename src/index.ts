8a5cde00cee63797ef38d570c97bf6bd19ff1af5
Olfran Jiménez <olfran@gmail.com>
2020-04-19 20:20:35 +0200

    Add connections between the first node and the destination node

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,18 @@ import { Field } from "./field/field";
 import { Block } from "./field/block";
 import { UndirectedGraph } from "./graph/UndirectedGraph";
 
-const DEST = '10_8';
-const START = '0_10';
+const DEST = '20_8';
+const START = '0_0';
+
 
 window.addEventListener('load', () => {
     const graph = new UndirectedGraph<Block>();
     const field = new Field(<SVGElement>document.querySelector('#paper'), graph);
-    field.allowDiagonals = !true;
-    field.blockSize = 50;
-    field.grid();
-    field.setBlock(10, 8, 'black', 'black');
-    field.setBlock(0, 10, 'blue', 'blue');
+    field.allowDiagonals = true;
+    field.blockSize = 30;
+    field.grid('#f1f1f1', '#ccc');
+    field.setBlockByKey(DEST, 'black', 'black');
+    field.setBlockByKey(START, 'blue', 'blue');
     document.getElementById('bfs').addEventListener('click', () => {
         graph.dijkstra(START, DEST, async (vertex) => {
             return true;
@@ -20,15 +21,17 @@ window.addEventListener('load', () => {
             console.log('%cFinished!', 'color: green');
             let current = shortestPath.get(DEST);
             let currentKey = DEST;
-            console.log(current.weight)
+            const connections = [currentKey];
             while(currentKey !== START) {
                 currentKey = current.previous;
                 current = shortestPath.get(currentKey);
-                const split = currentKey.split('_');
-                field.setBlock(Number(split[0]), Number(split[1]), 'green', 'black');
+                connections.push(currentKey);
+                //field.setBlockByKey(currentKey, 'green', 'black');
             }
+            field.showConnections(connections);
         });
     });
+    setupMenu(field);
 });
 
 async function Sleep(ms: number) {
@@ -37,4 +40,11 @@ async function Sleep(ms: number) {
             resolve();
         }, ms);
     });
+}
+
+function setupMenu(field: Field) {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    field.onBlockClick = (event: MouseEvent) => {
+        console.log(event.x, event.y);
+    };
 }
